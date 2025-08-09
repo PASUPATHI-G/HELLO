96ab788c049d4db94e4f50dbdd9ce8674f7d6950
PASUPATHI-G <pasupathigurusamy7@gmail.com>
2025-08-09 11:32:40 +0530

    09-08-2025

--- vite-pro/src/App.tsx
+++ vite-pro/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/img/logo.png'
+// import logo from './assets/img/thumbnail.mp4'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div>
+        <div className="four_zero_four_bg_404"></div>
+        <img src={logo} alt="text" />
+          
+      </div>
     </>
   )
 }
