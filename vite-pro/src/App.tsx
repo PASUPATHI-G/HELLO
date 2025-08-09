d8a6383315301891f1dea53b457e8b9cb1ffa944
PASUPATHI-G <pasupathigurusamy7@gmail.com>
2025-08-09 18:18:06 +0530

    Messy things

--- vite-pro/src/App.tsx
+++ vite-pro/src/App.tsx
@@ -1,8 +1,10 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import logo from './assets/img/logo.png'
+import "./App.css";
+import logo from "./assets/img/logo.png";
+import top from "./assets/img/top.png";
+import top2 from "./assets/img/top2.png";
 // import logo from './assets/img/thumbnail.mp4'
 
 function App() {
@@ -10,13 +12,34 @@ function App() {
 
   return (
     <>
+      <div className="layout-topbar .layout-topbar-sticky">
+        <div className="layout-topbar-inner">
+          <div className="layout-topbar-logo-container">
+            <img src={top2} alt="logo" />
+          </div>
+          {/* <div className="layout-topbar-logo">
+              <img src={top} alt="logo" />
+          </div> */}
+          <ul className="flex m-0 p-0 gap-2 align-items-center">
+            <li>
+              H
+            </li>
+            <li>
+              M
+            </li>
+            <li>
+              Y
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div>
-        <div className="four_zero_four_bg_404"></div>
+        {/* <div className="four_zero_four_bg_404"></div> */}
         <img src={logo} alt="text" />
-          
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
